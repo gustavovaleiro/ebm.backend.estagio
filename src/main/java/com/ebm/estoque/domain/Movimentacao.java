6cdcf1591bf78496965667ff0b141f90956f3a5a
Gustavo Valeiro <gustavovaleiro.cc@gmail.com>
2019-05-14 15:13:53 -0300

    n lembro

--- src/main/java/com/ebm/estoque/domain/Movimentacao.java
+++ src/main/java/com/ebm/estoque/domain/Movimentacao.java
@@ -6,23 +6,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.ebm.auth.Usuario;
 import com.ebm.estoque.domain.enums.TipoMovimentacao;
-import com.ebm.estoque.domain.interfaces.ProdutoVendas;
 
 @Entity
 public  class Movimentacao implements Serializable{
